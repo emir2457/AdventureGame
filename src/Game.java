377e9c62d0e6ccf9aef4af71c2b95a03b285cb85
emir2457 <emirhanyakiskan@outlook.com>
2024-05-30 14:25:16 +0300

    proje oluşturuldu

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Game {
+
+    Player player;
+    Location location;
+
+    public void login(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Macera Oyununa Hoşgeldiniz ! ");
+        System.out.println("İsminizi Giriniz: ");
+        String playerName = scan.nextLine();
+    }
+        
+    
+
+}
